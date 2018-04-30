32fbfb22ac08df5ac60f95c0ffce096d34729f36
Tom Rooker <tomdrooker@gmail.com>
2018-04-30 21:32:34 +0100

    Fifth commit.

--- src/components/Tracklist/Tracklist.js
+++ src/components/Tracklist/Tracklist.js
@@ -4,9 +4,17 @@ import { Track } from '../Track/Track';
 
 export class Tracklist extends React.Component {
   render() {
+    if (typeof this.props.tracks === 'undefined') {
+           let track = [{
+               name: 'Search did not return results',
+               artist: 'Try some different Search Terms',
+               album: ' '
+           }];
+         };
     return (
       <div className="TrackList">
         // You will add a map method that renders a set of Track components
+        console.log(this.props.tracks);
         {
           this.props.tracks.map(track => {
             return <Track track={track} key={track.id} />
