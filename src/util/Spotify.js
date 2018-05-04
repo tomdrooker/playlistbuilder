8e9f399d0b6a81c4114b5a594d65feaf7ea62495
Tom Rooker <tomdrooker@gmail.com>
2018-05-04 17:15:29 +0100

    Commit 10

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,10 +1,24 @@
 const accessToken = '';
+const expiresIn = '';
+const clientId = '';
+const redirectUri = 'http://localhost:3000/';
+const responseType = 'token';
 
 
 const Spotify = {
-  getAccessToken() {
+  const url = 'https://accounts.spotify.com/authorize';
+  const authorizeUrl = this.url + '/?client_id=' + clientId + '&response_type=' + responseType + '&redirect_uri=' + redirectUri;
 
-  }
-};
+  getAccessToken() {
+    if (accessToken) {
+      return accessToken;
+    } else if (window.location.href.match('/access_token=([^&]*)/', '/expires_in=([^&]*)/') {
+      const accessToken = window.location.href.match('/access_token=([^&]*)/');
+      const expiresIn = window.location.href.match('/expires_in=([^&]*)/');
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
+    } else if !(window.location.href.match('/access_token=([^&]*)/') {
+      window.location = this.authorizeUrl;
+    }
 
 export default Spotify;
