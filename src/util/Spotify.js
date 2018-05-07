dda1e5555b2100b5d596f4dc31dbcd2205828ff6
Tom Rooker <tomdrooker@gmail.com>
2018-05-07 16:24:32 +0100

    Commit to submit

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -14,7 +14,7 @@ export const Spotify = {
       accessToken = accessTokenMatch[1];
       const expiresIn = Number(expiresInMatch[1]);
       window.setTimeout(() => accessToken = '', expiresIn * 1000);
-      window.history.pushState('Access Token', null, '/'); // This clears the parameters, allowing us to grab a new access token when it expires.
+      window.history.pushState('Access Token', null, '/');
       return accessToken;
     } else {
       const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
@@ -24,6 +24,7 @@ export const Spotify = {
 
   search(term) {
     const accessToken = Spotify.getAccessToken();
+
     return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
       headers: {
         Authorization: `Bearer ${accessToken}`
